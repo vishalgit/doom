222bfc5c09b2170f46152fd405a36582041cb61c
Vishal <vishal.reply@gmail.com>
2026-01-22 03:23:03 +0530

    Updated literal config for configuration

---
+++ configs/fonts.el
@@ -0,0 +1,22 @@
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font
+      (font-spec :family "JetBrainsMono Nerd Font Mono"
+                 :size 20
+                 :weight 'semibold))
+;;      doom-variable-pitch-font (font-spec :family "JetBrainsMono Nerd Font" :size 18))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
---
+++ configs/personal.el
@@ -0,0 +1,6 @@
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+(setq user-full-name "Vishal Saxena"
+      user-mail-address "vishal.reply@gmail.com")
